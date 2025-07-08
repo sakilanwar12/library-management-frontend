4f05dd11638747322f053b084848c423c53977c2
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2025-07-09 00:13:43 +0600

    add - form

--- app/components/features/forms/AddNewBookForm/index.tsx
+++ app/components/features/forms/AddNewBookForm/index.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import  { forwardRef } from "react";
+import { Checkbox } from "~/components/ui/checkbox";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { Textarea } from "~/components/ui/textarea";
 
-function AddNewBookForm() {
+const AddNewBookForm = forwardRef<HTMLInputElement>((_, ref) => {
   return (
-    <div>AddNewBookForm</div>
-  )
-}
+    <form
+      className="space-y-3.5"
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log("Hello world");
+      }}
+    >
+      <Input placeholder="Title" type="text" />
+      <Input placeholder="Author" type="text" />
+      <Input placeholder="Genre" type="text" />
+      <Input placeholder="isbn" type="text" />
+      <Textarea placeholder="Description" />
+      <Input placeholder="Copies" type="number" />
+      <div className="flex items-center gap-1">
+        <Checkbox id="available" />
+        <Label htmlFor="available">Available</Label>
+      </div>
+      <input type="submit" ref={ref} className="hidden" />
+    </form>
+  );
+});
 
-export default AddNewBookForm
+AddNewBookForm.displayName = "AddNewBookForm";
+
+export default AddNewBookForm;
--- app/components/features/modals/AddNewBook.tsx
+++ app/components/features/modals/AddNewBook.tsx
@@ -9,38 +9,37 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "~/components/ui/dialog";
-import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
 import AddNewBookForm from "../forms/AddNewBookForm";
+import { useRef } from "react";
 
 export function AddNewBook() {
+  const buttonRef = useRef<HTMLInputElement>(null);
+
   return (
     <Dialog>
-      <form>
-        <DialogTrigger asChild>
-          <Button
-            variant="outline"
-            className="text-sm font-medium hover:text-blue-500 border-0 rounded-none"
-          >
-            Add Book
+      <DialogTrigger asChild>
+        <Button
+          variant="outline"
+          className="text-sm font-medium hover:text-blue-500 border-0 rounded-none"
+        >
+          Add Book
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle> Add New Book</DialogTitle>
+          <DialogDescription>Add a new book to your library</DialogDescription>
+        </DialogHeader>
+        <AddNewBookForm ref={buttonRef} />
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <Button type="button" onClick={() => buttonRef.current?.click()}>
+            Save changes
           </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle> Add New Book</DialogTitle>
-            <DialogDescription>
-              Add a new book to your library
-            </DialogDescription>
-          </DialogHeader>
-          <AddNewBookForm />
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </form>
+        </DialogFooter>
+      </DialogContent>
     </Dialog>
   );
 }
